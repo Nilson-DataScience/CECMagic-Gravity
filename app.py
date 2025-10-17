fec71c54bf42e4c619405e9b922d91a9b49b0020
Nilson-DataScience <nilsongithub@gmail.com>
2025-10-17 11:29:43 -0300

    update

---
+++ app.py
@@ -0,0 +1,72 @@
+player = {
+    '1': {
+        'nome':'Nilson',
+        'idade': 24,
+        'ryos': 320.50,
+        'classe': 'Mago',
+        'familia': 'Loki',
+        'habilidades': {
+            'ataque': 10,
+            'defesa': 5,
+            'vida': 10,
+        },
+        'nivel': 1,
+        'xp': 0,
+        'equipamentos': [2],
+        'dungeon': 1,
+
+    },
+    '2': {}
+}
+
+equipamentos = {
+    '1': {
+        'nome': 'Espada de Madeira',
+        'classe': 'Guerreiro',
+        'nivel': 1,
+        'habilidades': {
+            'ataque': 1,
+            'defesa': 1,
+            'vida': 0,}
+        },
+    '2': {
+        'nome': 'Livro Velho',
+        'classe': 'Mago',
+        'nivel': 1,
+        'habilidades': {
+            'ataque': 1,
+            'defesa': 0,
+            'vida': 1,}
+        },
+    '3': {
+        'nome': 'Arco de Madeira',
+        'classe': 'Arqueiro',
+        'nivel': 1,
+        'habilidades': {
+            'ataque': 2,
+            'defesa': 0,
+            'vida': 0,}
+        },
+}
+
+monstros = {
+    1: {'nome': 'Rato',
+        'habilidades': {
+            'ataque': 1,
+            'defesa': 0,
+            'vida': 1,}
+        },
+    2:{'nome': 'Formiga',
+        'habilidades': {
+            'ataque': 2,
+            'defesa': 2,
+            'vida': 2,}
+        },
+    3:{
+        'nome': 'Coelho',
+        'habilidades': {
+            'ataque': 4,
+            'defesa': 3,
+            'vida': 1,}
+    }
+    }
